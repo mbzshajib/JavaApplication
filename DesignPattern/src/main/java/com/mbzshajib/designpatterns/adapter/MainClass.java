12766688a6c79ad234c90d7f3ef8ac0a9d68cf8c
Zaman Shajib <mbzshajib@gmail.com>
2015-08-18 11:45:46 +0600

    [0.0.1] - initial setup

---
+++ DesignPattern/src/main/java/com/mbzshajib/designpatterns/adapter/MainClass.java
@@ -0,0 +1,10 @@
+package com.mbzshajib.designpatterns.adapter;
+
+/**
+ * Created by Shajib on 8/18/2015.
+ */
+public class MainClass {
+    public static void main(String[] args) {
+        System.out.println("Adapter Pattern Testing.");
+    }
+}
